829fa1ae61cf268fea1f4a1bfd04df190d3093a5
Samu <65758544+SmuuSka@users.noreply.github.com>
2021-11-01 09:58:11 +0200

    upd

--- PunkinParty2/Assets/Enemy/Enemy.cs
+++ PunkinParty2/Assets/Enemy/Enemy.cs
@@ -1,13 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class Enemy : MonoBehaviour
 {
     [SerializeField] private float moveSpeed;
+    private Rigidbody2D enemyRb;
+
+    private Vector3 direction;
+
+    private void Start()
+    {
+        enemyRb = GetComponent<Rigidbody2D>();
+    }
 
     private void Update()
     {
-        transform.position += transform.right * moveSpeed * Time.deltaTime;
+        GetMovementDirection();
+    }
+
+    private void GetMovementDirection()
+    {
+        direction = new Vector3(1f, -0.5f);
+        transform.position += direction * moveSpeed * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.attachedRigidbody.CompareTag("Player"))
+        {
+            Debug.Log("Osuma");
+            Destroy(gameObject);
+        }
+    }
+
+    private void DoDamage()
+    {
+
     }
 }
