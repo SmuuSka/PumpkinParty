d84227c72d87def92602578c3ca3e5b626a53468
Samu <65758544+SmuuSka@users.noreply.github.com>
2021-11-04 14:44:53 +0200

    up

---
+++ PunkinParty2/Assets/Map.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour
+{
+    void Update()
+    {
+        Vector3 pos = GameObject.Find("MainPlayer").transform.position;
+        pos.z = 1;
+        transform.position = pos;
+        
+        
+    }
+}
